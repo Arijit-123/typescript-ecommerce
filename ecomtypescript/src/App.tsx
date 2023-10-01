597f8654e674b93568719f14c0c2d66cae75e3ce
Arijit-123 <1603074@kiit.ac.in>
2023-10-01 20:17:09 +0530

    first commit

---
+++ ecomtypescript/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import { Container } from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <Routes>
+        <Route></Route>
+        </Routes>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
